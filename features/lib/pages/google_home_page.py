f003ff25192f9b9c0ab2682e2eb8178b96d1be0a
Arkadiy Voronov <voronov_ao@mail.ru>
2019-05-11 10:13:53 +0500

    add file

---
+++ features/lib/pages/google_home_page.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+from base_page_object import BasePage
+
+
+class GoogleHomePage(BasePage):
+
+    def __init__(self, contex):
+        BasePage.__init__(
+            self,
+            contex.browser,
+            base_url='https://google.com')
+
+    locator_dictionary = {
+        "for_sale_tab": (By.LINK_TEXT, 'For sale'),
+        "search_box": (By.ID, 'search-input-location'),
+        "search_button:": (By.ID, "search-submit")
+    }
+    
+    def type_search(self, word):
+        print(word)
+        self.search_box.send_keys(word)
